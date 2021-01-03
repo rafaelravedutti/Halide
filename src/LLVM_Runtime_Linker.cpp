adf3f8d5959ba8b497c013af3ed2c07861069c89
Rafael Ravedutti <rafaelravedutti@gmail.com>
2021-01-03 03:08:19 +0100

    Add PAPI profiler to runtime linker
    Signed-off-by: Rafael Ravedutti <rafaelravedutti@gmail.com>

--- src/LLVM_Runtime_Linker.cpp
+++ src/LLVM_Runtime_Linker.cpp
@@ -980,6 +980,7 @@ std::unique_ptr<llvm::Module> get_initial_module_for_target(Target t, llvm::LLVM
                     modules.push_back(get_initmod_windows_profiler(c, bits_64, debug));
                 } else {
                     modules.push_back(get_initmod_profiler(c, bits_64, debug));
+                    modules.push_back(get_initmod_papi_profiler(c, bits_64, debug));
                 }
             }
 
